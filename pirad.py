8ce98abff6c8350a337adc30563bbb18421ef405
Tims777 <timzi777@gmail.com>
2021-02-06 10:41:21 +0100

    Replaced flask by http.server for now

--- pirad.py
+++ pirad.py
@@ -1,24 +1,76 @@
-from flask import Flask, redirect, url_for
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 from omxplayer.player import OMXPlayer
 
-radio_title = 'SWR1'
-radio_url = 'https://swr-swr1-bw.cast.addradio.de/swr/swr1/bw/aac/96/stream.aac'
 
-player = OMXPlayer(radio_url, args=['-o', 'both'])
-app = Flask(__name__)
+class RadioPlayer():
 
-@app.route('/')
-def overview():
-    return f'Currently playing {radio_title} at volume {player.volume()}'
+    stations = [
+        ('SWR1', 'https://swr-swr1-bw.cast.addradio.de/swr/swr1/bw/aac/96/stream.aac'),
+        ('SWR3', 'https://swr-swr3-live.cast.addradio.de/swr/swr3/live/aac/96/stream.aac')
+    ]
+
+    def play(self):
+        if self.omx:
+            self.omx.play()
+        else:
+            self.omx = OMXPlayer(self.current_station_url, args=['-o', 'both'])
+
+    def pause(self):
+        if self.omx:
+            self.omx.pause()
+
+    def stop(self):
+        if self.omx:
+            self.omx.stop()
+            self.omx = None
+
+    current_station_id = 0
+
+    @property
+    def current_station_name(self):
+        return self.stations[self.current_station_id][0]
+
+    @property
+    def current_station_url(self):
+        return self.stations[self.current_station_id][1]
+
+    def next_station(self):
+        self.current_station_id += 1
+        self.current_station_id %= len(self.stations)
+        if self.omx:
+            self.omx.load(self.current_station_url)
 
 
-@app.route('/+')
-def volume_plus():
-    player.set_volume(player.volume() + 0.1)
-    return redirect(url_for('overview'))
+player = RadioPlayer()
 
 
-@app.route('/-')
-def volume_minus():
-    player.set_volume(player.volume() - 0.1)
-    return redirect(url_for('overview'))
+class RemoteControlHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/next':
+            player.next_station()
+            self.send_response(303)
+            self.send_header('Location', '/')
+            self.end_headers()
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'<html>')
+            self.wfile.write(b'<head><meta charset="UTF-8"><title>Pirad</title></head>')
+            self.wfile.write(b'<body>')
+            self.wfile.write(bytes(f'<p>Currently playing: {player.current_station_name}</p>', 'utf-8'))
+            self.wfile.write(b'<p><a href="/next">Next Station</a></p>')
+            self.wfile.write(b'</body>')
+            self.wfile.write(b'</html>')
+
+
+if __name__ == '__main__':
+    address = ('', 80)
+    server = ThreadingHTTPServer(address, RemoteControlHandler)
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        player.stop()
+        server.server_close()
