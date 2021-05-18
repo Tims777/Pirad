8b692002bc2d1e095de99dda515fcceed9c94ea4
Tims777 <timzi777@gmail.com>
2021-05-18 10:38:57 +0200

    Fix reference to current_station_id attribute

--- gpio.py
+++ gpio.py
@@ -9,7 +9,7 @@ class ButtonControl():
         self.player = player
         self.buttons = [Button(p) for p in pins]
         if len(self.buttons) == 2:
-            self.buttons[0].when_pressed = lambda: self.player.switch_station(self.player.station_id + 1)
-            self.buttons[1].when_pressed = lambda: self.player.switch_station(self.player.station_id - 1)
+            self.buttons[0].when_pressed = lambda: self.player.switch_station(self.player.current_station_id + 1)
+            self.buttons[1].when_pressed = lambda: self.player.switch_station(self.player.current_station_id - 1)
         else:
             raise NotImplementedError("Currently only a two-button-layout is supported.")
