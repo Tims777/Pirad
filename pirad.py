f9ff392f24a08210434405696eb0fc136a1da0a4
Tims777 <timzi777@gmail.com>
2021-05-18 10:33:54 +0200

    Add experimental support for GPIO buttons

--- pirad.py
+++ pirad.py
@@ -4,16 +4,21 @@ import asyncio
 
 from player import RadioPlayer
 from remotecontrol import RemoteControlSocket
+from gpio import ButtonControl
 
 # list of acceptable origins
 # * this is only to prevent xss-attacks (and does not diallow connections with a "faked" origin)
 # * 'null' is sent by firefox instead of 'file://'
 origins = ['http://192.168.0.77', 'file://', 'null']  # null is sent for local files opened in the browser
 
+# GPIO pins of the buttons
+pins = [17, 18]
+
 
 if __name__ == '__main__':
     player = RadioPlayer()
     rc = RemoteControlSocket(player)
+    bt = ButtonControl(player, pins)
 
     try:
         player.play()
